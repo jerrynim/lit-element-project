f009d3730574e47b661c13e3d7aad8048a5a42df
제리님 <tgeddy3@gmail.com>
2020-10-20 18:20:37 +0900

    라우팅 설정

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
 import "./styles.css";
-import "./views/portfolio-detail";
+import "./pages/portfolio-detail";
+import "./pages/todo-view";
+import { Router } from "@vaadin/router";
+
+window.addEventListener("load", () => {
+  initRouter();
+});
+
+function initRouter() {
+  const router = new Router(document.querySelector("main"));
+  router.setRoutes([
+    {
+      path: "/",
+      component: "todo-view",
+    },
+    {
+      path: "/portfolio-detail",
+      component: "portfolio-detail",
+      action: () => {
+        import("./pages/portfolio-detail");
+      },
+    },
+    {
+      path: "(.*)",
+      component: "not-found-view",
+      action: () => import("./pages/portfolio-detail"),
+    },
+  ]);
+}
